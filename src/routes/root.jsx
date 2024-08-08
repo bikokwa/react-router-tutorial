5b0b67e23d6382f940b891fbd348296a7ca92e41
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-08-08 08:56:02 +0300

    load data

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,14 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLoaderData } from "react-router-dom";
+import { getContacts } from "../contacts";
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
 
 export default function Root() {
+  const { contacts } = useLoaderData();
+
   return (
     <>
       <div id="sidebar">
